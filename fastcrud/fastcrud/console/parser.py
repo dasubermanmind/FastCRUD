02a7350c140f1f4bc81ce2888be52a549c1afb7e
dasubermanmind <ero33@drexel.edu>
2023-09-01 09:15:26 -0400

    boilerplate for parser

--- fastcrud/fastcrud/console/parser.py
+++ fastcrud/fastcrud/console/parser.py
@@ -1,18 +1,32 @@
-
+import argparse
+from pathlib  import Path
 
 class Parser:
 
     def __init__(self):
-        pass
+        self.parser = argparse.ArgumentParser()
+        self.target_path = '' # Add settings for file path
 
-    def parse(self):
-        pass
+    # This will in the future also take care of adding migration files, updating schemas etc...
+    def add_arguments(self, param: str):
+        if param == '--m':
+            self.parser.add_argument('--m' ,dest=param, type=str, help='Create a Model instance')
+        elif param == '--c':
+            self.parser.add_argument('--c' ,dest=param, type=str, help='Create a Controller instance')
+        elif param == '--g':
+            self.parser.add_argument('--g' ,dest=param, type=str, help='Create a Generator to create everything')
 
-    def name(self):
-        pass
 
-    def parameters(self):
-        pass
+    def parser(self):
+        args = self.parser.parse_args()
+        return args
 
-    def parse_options(self):
-        pass
+    def build_dir_and_models(self, model='', controller='', generator=''):
+        if model != '':
+            if Path.exists(self.target_path):
+                print('The Model directory already exists')
+            else:
+                pass
+                # TODO: Create the file path
+                # TODO: Factory pattern. Create the model instance
+                
