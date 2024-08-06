c9d3a4d4feaf83706fdff616ff52eddc0f2f6eeb
dasUbermanmind <eliot.ost@protonmail.com>
2024-08-06 11:30:44 -0400

    Generator now supports model, CRUD & router creation

--- fastcrud/fastcrud/core.py
+++ fastcrud/fastcrud/core.py
@@ -1,31 +1,69 @@
 
 
 import os
+from typing import List
 import typer
+from api import Generator
+
 
 app = typer.Typer()
 
 
 @app.command()
 def model(name: str):
+    genny = Generator()
     name = name.capitalize()
-    # TODO: Update both
+    model_created = genny.generate_model_content()
     model_path = os.path.join("models", f"{name.lower()}.py")
+    
     with open(model_path, "w") as f:
         f.write(model_created)
     print(f"Model Created: {name} at {model_path}")
 
     crud_path = os.path.join("crud", f"{name.lower()}.py")
+    crud_actions = genny.generate_crud_content()
+
+    with open(crud_path, "w") as f:
+        f.write(crud_actions)
+    print(f'Crud Created: {name.lower()} at {crud_path}')
+    print("\nEnjoy!\n")
+
+
+@app.command()
+def all(name: str, properties: List[str]=typer.Option([], "--property", "-p")):
+    genny = Generator()
+    name = name.capitalize()
+    model_created = genny.generate_model_content()
+    model_path = os.path.join("models", f"{name.lower()}.py")
+    
+    # Model
+    with open(model_path, "w") as f:
+        f.write(model_created)
+    print(f"Model Created: {name} at {model_path}")
+
+    crud_path = os.path.join("crud", f"{name.lower()}.py")
+    crud_actions = genny.generate_crud_content()
+
     with open(crud_path, "w") as f:
         f.write(crud_actions)
     print(f'Crud Created: {name.lower()} at {crud_path}')
 
-    # Update the main>?
-    # if db_arg:
-    #     db_arg.name = arg_udpate
-    #     db.commit()
-    #     db.refresh(db_arg)
-    # return db_arg
+    # Crud
+    model_created = genny.generate_model_content()
+    model_path = os.path.join("models", f"{name.lower()}.py")
+    
+    with open(model_path, "w") as f:
+        f.write(model_created)
+    print(f"Model Created: {name} at {model_path}")
+
+    # Router
+    router_path = os.path.join("routers", f"{name.lower()}.py")
+    router_actions = genny.generat_router_content()
+
+    with open(router_path, "w") as f:
+        f.write(router_actions)
+    print(f'Router Created: {name.lower()} at {router_path}')
+    print("Don't forget to include this to your Main.py")
 
 
 
--- test.py
+++ test.py
@@ -1,13 +1,3 @@
-from fastcrud.fastcrud.console.parser import Parser
-import argparse
 
-if __name__ == '__main__':
-    print('Testing FastCRUD')
-    parser = argparse.ArgumentParser()
-    
-    Parser.add_arguments(parser)
-    __args = Parser.parser(parser)
-    print(f'Args-->{type(__args)}')
-    
-    Parser.build_dir_and_models(__args.m)
 
+python core.py all User -p age -p email -p address
