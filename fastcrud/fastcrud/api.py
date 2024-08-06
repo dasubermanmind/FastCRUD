50c03f8bbd24387d6fe5d735d93b23dbb037732a
dasUbermanmind <eliot.ost@protonmail.com>
2024-08-06 11:18:39 -0400

    Added Router Generation

--- fastcrud/fastcrud/api.py
+++ fastcrud/fastcrud/api.py
@@ -57,6 +57,61 @@ def delete_{name.lower()}(db: Session, arg_id: int):
         return crud
 
     def generat_router_content(name: str, properties: List[str]):
-        pass
+        router = f"""
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from models.{name.lower()} import {name}
+from crud.{name.lower()} import create_{name.lower()}, get_{name.lower()}, 
+update_{name.lower()}, delete_{name.lower()}
+from db.session import SessionLocal
+from viz impot Viz
+
+router = APIRouter()
+viz = Viz("db_connection_string_here")
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@viz.viz
+@router.post("/{name.lower()}s/", response_model={name})
+def create_item({', '.join([f"{prop}: str" for prop in properties])}, db: Session = Depends(get_db)):
+    item = {name}({', '.join([prop for prop in properties])})
+    return create_{name.lower()}(db=db, item=item)
+
+
+@viz.viz
+@router.get("/{name.lower()}s/{{item_id}}", response_model={name})
+def read_item(item_id: int, db: Session = Depends(get_db)):
+    db_item = get_{name.lower()}(db, item_id=item_id)
+    if db_item is None:
+        raise HTTPException(status_code=404, details="Not Found")
+    
+    return db_item
+
+
+@viz.viz
+@router.put("/{name.lower()}s/{{item_id}}", response_model={name})
+def update_item(item_id: int, {', '.join([f"{prop}: str" for prop in properties])}, db: Session = Depends(get_db)):
+    item = {name}({', '.join([prop for prop in properties])})
+    db_item = update_{name.lower()}(db, item=item_id, item=item)
+    if db_item is None:
+        raise HTTPException(status_code=404, details="Not Found")
+    
+    return db_item
+
+@viz.viz
+@router.delete("/{name.lower()}s/{{item_id}}", response_model={name})
+def delete_item(item_id: int, db: Session = Depends(get_db)):
+    db_item = delete_{name.lower()}(db, item_id=item_id)
+    if db_item is None:
+        raise HTTPException(status_code=404, details="Not Found")
+    
+    return db_item
+        """
+        return router
 
     
